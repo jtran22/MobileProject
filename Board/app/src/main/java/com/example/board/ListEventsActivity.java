2e922c4ce8a6486393f0e88a7c63dc402f47c19d
Harry Haisty <harryhaisty@Harrys-MacBook-Pro.local>
2019-09-11 16:37:33 -0400

    adds rest of default activity pages

---
+++ Board/app/src/main/java/com/example/board/ListEventsActivity.java
@@ -0,0 +1,14 @@
+package com.example.board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_events);
+    }
+}
