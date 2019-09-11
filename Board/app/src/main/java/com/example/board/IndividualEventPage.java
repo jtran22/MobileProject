25a78e79f4349e410f94b4d4cb87b3465a07430e
Harry Haisty <harryhaisty@Harrys-MacBook-Pro.local>
2019-09-11 16:21:04 -0400

    adds major activity pages (blank)

---
+++ Board/app/src/main/java/com/example/board/IndividualEventPage.java
@@ -0,0 +1,14 @@
+package com.example.board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualEventPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_event_page);
+    }
+}
