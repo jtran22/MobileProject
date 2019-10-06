3e8f2e4b679f8ffc03b9562a877f11b311ae894e
tinyturtlestudio <codyharrison@comcast.net>
2019-10-06 17:25:27 -0400

    Firebase Authentication

--- Board/app/src/main/java/com/example/board/SignInActivity.java
+++ Board/app/src/main/java/com/example/board/SignInActivity.java
@@ -7,18 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignInActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    Intent loginIntent;
+    Intent signupIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser!=null){
+            //loginIntent = new Intent(this, HomePage.class)
+            //startActivity(loginIntent)
+        }
+
         TextView t2 = findViewById(R.id.tvLinkSignUp);
 
         t2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent loginIntent = new Intent(v.getContext(), SignUpActivity.class);
-                startActivity(loginIntent);
+                signupIntent = new Intent(v.getContext(), SignUpActivity.class);
+                startActivity(signupIntent);
             }
         });
 
