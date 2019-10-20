647826904a88534e8f709020f12e9866e4d5c441
tinyturtlestudio <codyharrison@comcast.net>
2019-10-20 00:10:14 -0400

    Event List Spinner

--- Board/app/src/main/java/com/example/board/ListEventsActivity.java
+++ Board/app/src/main/java/com/example/board/ListEventsActivity.java
@@ -6,7 +6,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -14,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,6 +21,10 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListEventsActivity extends AppCompatActivity {
     private TextView tvLat;
@@ -40,6 +44,10 @@ public class ListEventsActivity extends AppCompatActivity {
 
     final private int LOCATION_PERMISSION_REQUEST_CODE = 103;
 
+    private static final String TAG = "ListEvents";
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList <String> mImagesURL = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +61,10 @@ public class ListEventsActivity extends AppCompatActivity {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         getLocationPermission();
         distance = 7.2;
+
+        initImageBitmaps();
+        Spinner spinner = findViewById(R.id.spinner);
+
 
         sbDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -79,6 +91,46 @@ public class ListEventsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initImageBitmaps()
+    {
+        Log.d(TAG,"initImagesBitmaps: preparing Bitaps");
+        mImagesURL.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImagesURL.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImagesURL.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImagesURL.add("https://i.redd.it/j6myfqglup501.jpg");
+        mNames.add("Rocky Mountain National Park");
+
+
+        mImagesURL.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImagesURL.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+
+        mImagesURL.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView()
+    {
+        Log.d(TAG,"initRecyclerView: inti recycler");
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,mNames,mImagesURL);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 
     private void getBoundingCoord(){
         boundingCoords = myLocation.boundingCoordinates(distance,radius);
